d7800a7588d3b4c321338df80a59c734f61e1be7
Matt Gibson <git@mgibson.dev>
2024-02-04 16:12:39 -0500

    initial commit

---
+++ concurrently.nix
@@ -0,0 +1,30 @@
+{ pkgs ? import <nixpkgs> { }
+, stdenv ? pkgs. stdenv
+, lib ? stdenv.lib
+, rustPlatform ? pkgs.rustPlatform
+, fetchFromGitHub ? pkgs.fetchFromGitHub
+}:
+rustPlatform.buildRustPackage rec {
+  pname = "concurrently";
+  version = "0.3.3";
+
+  src = fetchFromGitHub {
+    owner = "tqwewe";
+    repo = "concurrently";
+    rev = "${version}";
+    hash = "sha256-XUcnSuWAbi8mWL2gAdfdUs74dt5BDY/RJgPlpJ+KQC4=";
+  };
+
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+    outputHashes = {
+    };
+  };
+  
+  meta = with lib; {
+    description = "A utility for executing multiple commands concurrently";
+    homepage = "https://github.com/tqwewe/concurrently";
+    changelog = "https://github.com/tqwewe/concurrently/blob/v${version}/CHANGELOG.md";
+    maintainers = with maintainers; [ tqwewe ];
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Flake providing rust concurrently";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; # We want to use packages from the binary cache
+    flake-utils.url = "github:numtide/flake-utils";
+    gitignore = { url = "github:hercules-ci/gitignore.nix"; flake = false; };
+  };
+
+  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
+  flake-utils.lib.eachSystem [ "x86_64-linux" ] (system: let
+    pkgs = nixpkgs.legacyPackages.${system};
+  in rec {
+    packages.concurrently = pkgs.callPackage ./concurrently.nix { inherit pkgs; };
+
+    legacyPackages = packages;
+
+    defaultPackage = packages.concurrently;
+
+    devShell = pkgs.mkShell {
+      CARGO_INSTALL_ROOT = "${toString ./.}/.cargo";
+    };
+  });
+}
